f69b1f57d4fa20d1e0c82fa4908e220702ff862f
Connor Hamilton Inglis <chinglis@umich.edu>
2019-08-20 23:23:23 -0400

    Updates

---
+++ precache-manifest.cee09b7bfe00375ba428dc9a03bb18e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3038740cc0aa84fb1b883b92b62e5a91",
+    "url": "/PLFantasyTool/index.html"
+  },
+  {
+    "revision": "d458402fc282b5ea3576",
+    "url": "/PLFantasyTool/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1649648a7649d959ea30",
+    "url": "/PLFantasyTool/static/js/2.3da40231.chunk.js"
+  },
+  {
+    "revision": "d458402fc282b5ea3576",
+    "url": "/PLFantasyTool/static/js/main.b5080f26.chunk.js"
+  },
+  {
+    "revision": "6b7f079b59a93e88bbf2",
+    "url": "/PLFantasyTool/static/js/runtime~main.6b5630b7.js"
+  }
+]);
